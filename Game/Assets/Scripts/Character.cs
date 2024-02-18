84d86fc1cd9e230e240e6799977681fb3b9e1919
alpkirisci <salpkirisci@gmail.com>
2024-02-18 05:55:33 +0300

    aiDone

--- Game/Assets/Scripts/Character.cs
+++ Game/Assets/Scripts/Character.cs
@@ -1,86 +1,53 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class Character : MonoBehaviour
 {
-    [SerializeField]
-    private float _speed;
-    public float Speed
-    {
-        get { return _speed; }
-        set { _speed = value; }
-    }
+    public int health;
+    public int maxHealth;
+    public int damage;
+    public string Name;
 
-    [SerializeField]
-    private int _health;
-    public int Health {  
-        get { return _health; }
-        set { _health = value; }
-    }
-
-    [SerializeField]
-    private int _maxHealth;
-    public int MaxHealth
-    {
-        get { return _maxHealth; }
-        set { _maxHealth = value; }
-    }
-
-    [SerializeField]
-    private int _damage;
-    public int Damage
-    {
-        get { return _damage; }
-        set { _damage = value; }
-    }
-
-    [SerializeField]
-    private string _name;
-    public string Name
-    {
-        get { return _name; }
-        set { _name = value; }  
-    }
-
-    private Rigidbody2D myBody;
-    private Animator anim;
-    private SpriteRenderer sr;
-    private string WALK_ANIMATON = "Walk";
+    public Rigidbody2D rigidBody;
+    public Animator animator;
+    public SpriteRenderer spriteRenderer;
+    public string WALK_ANIMATON = "Walk";
+    public string ATTACK_ANIMATION = "Attack";
 
 
     public Character() { }
-    public Character(float speed, int health, int maxHealth, int damage, string name)
+    public Character(int health, int maxHealth, int damage, string name)
     {
-        Speed = speed;
-        Health = health;
-        MaxHealth = maxHealth;
-        Damage = damage;
+        this.health = health;
+        this.maxHealth = maxHealth;
+        this.damage = damage;
         Name = name;
     }
 
     public virtual void attack(Character target)
     {
-        target.Health -= Damage;
+        target.health -= damage;
     }
 
     //
     private void Awake()
     {
-        myBody = GetComponent<Rigidbody2D>();
-        anim = GetComponent<Animator>();
-        sr = GetComponent<SpriteRenderer>();
+        rigidBody = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
 
     // Start is called before the first frame update
-    void Start()
+    public virtual void Start()
     {
         
     }
 
     // Update is called once per frame
-    void Update()
+    public virtual void Update()
     {
         
     }
